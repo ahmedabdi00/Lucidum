338a330693bb3fb0bbdd61193bd146e57a7a4e9f
ahmedabdi00 <117784475+ahmedabdi00@users.noreply.github.com>
2023-03-16 19:18:24 -0400

    Lucidum Web App Update

---
+++ src/components/Hero.jsx
@@ -0,0 +1,53 @@
+import styles from "../style";
+import { discount, robot } from "../assets";
+import GetStarted from "./GetStarted";
+
+const Hero = () => {
+  return (
+    <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+        <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
+          <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
+          <p className={`${styles.paragraph} ml-2`}>
+            <span className="text-white">Get 20%</span> OFF Your{" "}
+            <span className="text-white">1'st month</span> with Lucidum
+          </p>
+        </div>
+
+        <div className="flex flex-row justify-between items-center w-full">
+          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]"> 
+            An Array of Digital Assets That Bring <br className="sm:block hidden" />{" "}
+            <span className="text-gradient">A World of Ease</span>{" "}
+          </h1>
+          <div className="ss:flex hidden md:mr-4 mr-0">
+            <GetStarted />
+          </div>
+        </div>
+
+        <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
+          To Your Buisiness.
+        </h1>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Here at Lucidum.Ai we know that its not easy to price so we take the burden
+        off your shoulders. We offer Simplicity, Accuracy, and as our name suggests Clarity!
+        </p>
+      </div>
+
+      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+        <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+
+        {/* gradient start */}
+        <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
+        <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
+        <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
+        {/* gradient end */}
+      </div>
+
+      <div className={`ss:hidden ${styles.flexCenter}`}>
+        <GetStarted />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/constants/index.js
@@ -0,0 +1,197 @@
+import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star  } from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Features",
+  },
+  {
+    id: "product",
+    title: "Product",
+  },
+  {
+    id: "clients",
+    title: "Clients",
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "(ANR Calculator) Average Nightly Rate",
+    content:
+      "The ultimate pricing tool for vacation rental owners, property managers, and hosts. Our calculator uses advanced AI and machine learning algorithms to deliver real-time pricing forecasts, helping you stay current and competitive in today's fast-paced rental market. With our ANR Calculator, you can instantly determine the optimal pricing for your rental property, based on a range of external factors such as seasonal demand, local events, and competitor rates. Our tool takes the guesswork out of pricing, allowing you to maximize your revenue and occupancy rates, while staying ahead of the competition. Try it out today and experience the power of AI in vacation rental pricing! ",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "(OC/VC) Occuoancy & Vacancy Rate Calculator",
+    content:
+      "The perfect solution for landlords, property investors, and real estate professionals looking to make data-driven decisions about their rental properties. Using a powerful combination of data scraping and AI algorithms, our tool generates accurate occupancy and vacancy rates for rental properties, based on booking calendars and other relevant data. Simply input the address of your rental property or prospective rental property, and our tool will provide you with a detailed report on its occupancy and revenue potential. With this information, you can make informed decisions about purchasing, pricing, and marketing your rental properties. Try it out today and unlock the full potential of your rental properties!",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "Customized Reports",
+    content:
+      "Our service delivers customized reports that provide valuable insights into the performance of your existing and potential rental units, helping you achieve your business goals. Our personalized reports are tailored to your specific needs, showcasing exceptional units and identifying areas where underperformance can be improved. Our AI-powered algorithms analyze a range of data points, including occupancy rates, revenue streams, and tenant feedback, to provide you with a comprehensive understanding of your rental units' performance. With our service, you'll be able to identify and address potential issues quickly and effectively, while optimizing the performance of your rental units to maximize revenue and tenant satisfaction. ",
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "I struggle to identify areas where my rental units are underperforming and could be improved.",
+    name: "Herman Jensen",
+    title: "Airbnb Superhost",
+    img: people01,
+  },
+  {
+    id: "feedback-2",
+    content:
+      "Managing multiple rental units can be time-consuming and overwhelming on such a small and large scale.",
+    name: "Steve Mark",
+    title: "Property Portfolio Manager",
+    img: people02,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "Finding ways to stand out in a crowded rental market can be a challenge.",
+    name: "Kenn Gallagher",
+    title: "VRBO Host",
+    img: people03,
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "Live-In all States",
+    value: "52",
+  },
+  {
+    id: "stats-2",
+    title: "Customer Support",
+    value: "24/7",
+  },
+  {
+    id: "stats-3",
+    title: "Transaction",
+    value: "",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "Content",
+        link: "https://www.hoobank.com/content/",
+      },
+      {
+        name: "How it Works",
+        link: "https://www.hoobank.com/how-it-works/",
+      },
+      {
+        name: "Create",
+        link: "https://www.hoobank.com/create/",
+      },
+      {
+        name: "Explore",
+        link: "https://www.hoobank.com/explore/",
+      },
+      {
+        name: "Terms & Services",
+        link: "https://www.hoobank.com/terms-and-services/",
+      },
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      {
+        name: "Help Center",
+        link: "https://www.hoobank.com/help-center/",
+      },
+      {
+        name: "Partners",
+        link: "https://www.hoobank.com/partners/",
+      },
+      {
+        name: "Suggestions",
+        link: "https://www.hoobank.com/suggestions/",
+      },
+      {
+        name: "Blog",
+        link: "https://www.hoobank.com/blog/",
+      },
+      {
+        name: "Newsletters",
+        link: "https://www.hoobank.com/newsletters/",
+      },
+    ],
+  },
+  {
+    title: "Partner",
+    links: [
+      {
+        name: "Our Partner",
+        link: "https://www.hoobank.com/our-partner/",
+      },
+      {
+        name: "Become a Partner",
+        link: "https://www.hoobank.com/become-a-partner/",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];
+
+export const clients = [
+  {
+    id: "client-1",
+    logo: airbnb,
+  },
+  {
+    id: "client-2",
+    logo: binance,
+  },
+  {
+    id: "client-3",
+    logo: coinbase,
+  },
+  {
+    id: "client-4",
+    logo: dropbox,
+  },
+];
